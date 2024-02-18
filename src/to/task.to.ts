36a218e530ee6f6ddd7fa9d332db22850fdb34f0
Tharindu Nuwan Madhushanka <tharindunm94@gmail.com>
2024-02-18 12:17:24 +0530

    Create TaskTo clas

---
+++ src/to/task.to.ts
@@ -0,0 +1,8 @@
+export class TaskTo {
+
+    constructor(public id: number,
+                public description: string,
+                public status: boolean,
+                public email: string) {
+    }
+}
