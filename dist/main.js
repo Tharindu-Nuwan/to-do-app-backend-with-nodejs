4cbd789425effca60e91957ed538143dcd4165c5
Tharindu Nuwan Madhushanka <tharindunm94@gmail.com>
2024-02-18 11:37:39 +0530

    Dispatch requests to Controller in main.ts

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,11 @@
-export {};
+import express, { json } from "express";
+import { TaskHttpController } from "./controller/task.http.controller.js";
+import cors from "cors";
+const app = express();
+app.use(json());
+app.use(cors());
+app.use('/api/v1/tasks', TaskHttpController);
+app.listen(8080, () => {
+    console.log("Server is listening to the port 8080");
+});
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,14 @@
-import express from "express";
+import express, {json} from "express";
+import {TaskHttpController} from "./controller/task.http.controller.js";
+import cors from "cors";
 
+const app = express();
+
+app.use(json());
+app.use(cors());
+
+app.use('/api/v1/tasks', TaskHttpController);
+
+app.listen(8080, ()=> {
+    console.log("Server is listening to the port 8080");
+});
